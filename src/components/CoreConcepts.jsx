9d57c9f1a42c0ac183a2da315eee8524d7afeae0
Ou Yang dev <ouyang1278@gmail.com>
2023-10-29 19:41:30 +0800

    refactors

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,13 @@
+import { CORE_CONCEPTS } from './../data'
+import CoreConcept from './CoreConcept';
+
+export default function CoreConcepts() {
+    return (
+        <section id="core-concepts">
+            <h2>Core Concept</h2>
+            <ul>
+                {CORE_CONCEPTS.map(item => <CoreConcept key={item.title} {...item} />)}
+            </ul>
+        </section>
+    )
+}
